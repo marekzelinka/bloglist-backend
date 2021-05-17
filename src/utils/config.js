20398146fd51a742773d35f387ec437e11593ca4
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-05-17 19:42:08 +0200

    setup test db

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const config = {
   PORT: process.env.PORT,
   MONGODB_URI: process.env.MONGODB_URI,
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI,
 }
 
 for (const [key, value] of Object.entries(config)) {
@@ -10,5 +11,8 @@ for (const [key, value] of Object.entries(config)) {
     throw new Error(`Define the ${key} env var.`)
   }
 }
+
+config.MONGODB_URI =
+  process.env.NODE_ENV !== 'test' ? config.MONGODB_URI : config.TEST_MONGODB_URI
 
 module.exports = config
