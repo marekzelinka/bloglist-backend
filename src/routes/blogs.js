680bc0d21374a2c2ec475a0031587f5df183753c
Marek Zelinka <mzelinka17@gmail.com>
2024-08-03 15:09:12 +0200

    solve ex4.17

--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -1,16 +1,24 @@
 import express from 'express'
 import { Blog } from '../models/blog.js'
+import { User } from '../models/user.js'
 
 export const blogsRouter = express.Router()
 
 blogsRouter.post('/', async (req, res) => {
-  const blog = new Blog(req.body)
+  // Probably the first user, idk... :D
+  const user = await User.findOne({})
+
+  const blog = new Blog({ ...req.body, user: user.id })
   const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   res.status(201).json(savedBlog)
 })
 
 blogsRouter.get('/', async (_req, res) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1 })
   res.json(blogs)
 })
 
